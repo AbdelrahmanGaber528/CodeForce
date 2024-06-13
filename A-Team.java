ba92e8794363e572842cce20248c393f707d3283
Abdelrahman Gaber <144827977+AbdelrahmanGaber528@users.noreply.github.com>
2024-06-13 23:08:54 +0300

    Update A-Team.java

--- A-Team.java
+++ A-Team.java
@@ -1,4 +1,3 @@
-
 import java.util.Scanner;
 public class A-Team {
   public static void main(String[] args) {
@@ -17,6 +16,5 @@ public class A-Team {
       }
     }
     System.out.println(values);
-
   }
 }
