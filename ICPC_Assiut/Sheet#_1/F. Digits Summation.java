e40bd8ae612b99e1eb72bffa5eb69563da26a45b
AbdelrahmanGaber528 <144827977+AbdelrahmanGaber528@users.noreply.github.com>
2024-06-20 19:01:33 +0300

    delete folders

--- ICPC_Assiut/Sheet#_1/F. Digits Summation.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-public class DigitsSummation{
-    public  static void main(String[] args) {
-    Scanner getNumber = new Scanner(System.in);
-    long N = getNumber.nextLong(),M = getNumber.nextLong();
-    long result = (N%10)+(M%10);
-        System.out.println(result);
-    }
-}
--- ICPC_Assiut/Sheet#_1/G. Summation from 1 to N.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-public class DigitsSummation{
-    public  static void main(String[] args) {
-    Scanner getNumber = new Scanner(System.in);
-    long num= getNumber.nextInt();
-    long result = (num*(num+1))/2;
-        System.out.println(result);
-    }
-}
