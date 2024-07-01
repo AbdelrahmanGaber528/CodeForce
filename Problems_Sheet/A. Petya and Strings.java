31c847c9f131821e06c4f11c5da6090d50c32e7e
Abdelrahman Gaber <144827977+AbdelrahmanGaber528@users.noreply.github.com>
2024-07-01 09:33:25 +0300

    Update A. Petya and Strings.java

--- Problems_Sheet/A. Petya and Strings.java
+++ Problems_Sheet/A. Petya and Strings.java
@@ -4,17 +4,17 @@ class Main{
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
-        String name1 = input.next().toUpperCase();
-        String name2 =input.next().toUpperCase();
+        String name1 = input.next();
+        String name2 =input.next();
 
-       if((name1.compareTo(name2))==0) {
-           System.out.println(0);
-       }
-       else if((name1.compareTo(name2))>0){
+        if((name1.compareToIgnoreCase(name2))==0) {
+            System.out.println(0);
+        }
+        else if((name1.compareToIgnoreCase(name2))>0){
             System.out.println(1);
-       }
-       else {
-           System.out.println(-1);
-       }
+        }
+        else {
+            System.out.println(-1);
+        }
 
     }}
