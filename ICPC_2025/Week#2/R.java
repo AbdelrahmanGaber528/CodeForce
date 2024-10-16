fdf6765fa56e8109a3387fff3ba72bebcb158f77
Abdelrahman Gaber <144827977+AbdelrahmanGaber528@users.noreply.github.com>
2024-10-16 14:01:00 +0300

    Update R.java

--- ICPC_2025/Week#2/R.java
+++ ICPC_2025/Week#2/R.java
@@ -1,22 +1,25 @@
 import java.util.Scanner;
 
-public class A{
+public class R {
     public static void main(String[] args) {
-        try (Scanner input = new Scanner(System.in)) {
-            int n = input.nextInt();
-            int m = input.nextInt();
+        Scanner scanner = new Scanner(System.in);
+        int T = scanner.nextInt();
 
-            // Start from the next even number if n is odd
-            if (n % 2 != 0) {
-                n++;
+        for (int t = 0; t < T; t++) {
+            int N = scanner.nextInt();
+            int count = 0;
+
+            for (int z = 0; Math.pow(z,4) * 4 <= N; z++) {
+                for (int y = 0; Math.pow(y,3) * 3 + Math.pow(z,4) * 4 <= N; y++) {
+                    for (int x = 0; Math.pow(x,2) * 2 + Math.pow(y,3) * 3 + Math.pow(z,4) * 4 <= N; x++) {
+                        double w = N - (Math.pow(x,2) * 2 + Math.pow(y,3) * 3 + Math.pow(z,4) * 4);
+                        if (w >= 0) {
+                            count++;
+                        }
+                    }
+                }
             }
-
-            StringBuilder evenNumbers = new StringBuilder();
-            for (int i = n; i <= m; i += 2) {
-                evenNumbers.append(i).append(" ");
-            }
-
-            System.out.print(evenNumbers.toString().trim());
+            System.out.println(count);
         }
     }
 }
